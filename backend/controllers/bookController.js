3e8dcad81e151ef975abea6acb30275d50996ff7
rajkumar-patidar <rajkumar.patidar62@gmail.com>
2025-12-26 21:23:05 +0530

    test code

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -19,7 +19,7 @@ exports.createBook = catchAsyncErrors(async (req, res, next) => {
       pdf_url: '1691513649039-.pdf',
     });
     await newBook.save();
-    test();
+    test(test);
     res.redirect('/');
 });
 
